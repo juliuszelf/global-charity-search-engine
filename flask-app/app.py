15448437cc21fa8d4efdb98fa7010a47744706ca
Julius vanaf Macbook Air <juliuszelf@gmail.com>
2019-11-24 23:25:32 +0100

    A simple Flask version in docker running

---
+++ flask-app/app.py
@@ -0,0 +1,13 @@
+# flask-app/app.py
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Flask in Docker container!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
+
