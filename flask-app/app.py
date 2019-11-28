11298b1404be017d5f4571055cf7a3b38e7221e4
Julius Huijnk <julius@immer.im>
2019-11-28 14:03:22 +0100

    Steps towards search bar in flask

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,9 +3,21 @@
 from flask import Flask
 app = Flask(__name__)
 
+'''
+TODO: confirm connection is ok
+from elasticsearch import Elasticsearch
+es = Elasticsearch('http://es01:9200')
+'''
+
 @app.route("/")
-def hello_world():
-    return "Flask in Docker container!!"
+def home():
+    return """
+    <form>
+      <label for="site-search">Global charity search:</label>
+      <input type="search" id="site-search" name="q" aria-label="Search charities">
+      <button>Search</button>
+    </form>
+    """
 
 
 if __name__ == "__main__":
