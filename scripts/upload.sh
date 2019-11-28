0b14a8a53a5f2fa74214bc0f8e00fa4b14ff226f
Julius Huijnk <julius@immer.im>
2019-11-28 11:10:37 +0100

    makefile creates parts for all sources and added script for upload-all to ES

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,9 +1,15 @@
 echo "Uploading file $1 parts to Elastic Search.."
 for i in $1-part-*; 
 do 
-   echo "Uploading file $i"
-   # "head -c100" ensures we only see first 100 characters of result, making it easier to see what happens
-   curl -r 0-199- -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary "@$i" | head -c100
+	# Only upload the parts, not the es.json itself
+	# $i includes the 'data/<country>/', hence the *
+	if [[ $i == *-part-* ]];
+	then
+        echo "Uploading file $i"
+
+		# "head -c100" ensures we only see first 100 characters of result, making it easier to see what happens
+		curl -r 0-199- -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary "@$i" | head -c100
+	fi
 done
 
 echo "Upload ended."
