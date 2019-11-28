90f0b8042a2376ae80cd264597ba2ee985d96a76
Julius vanaf Macbook Air <juliuszelf@gmail.com>
2019-11-28 09:49:01 +0100

    things i tried..

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,4 +1,9 @@
-echo "Uploading file $1 to Elastic Search.."
-curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary "@$1";echo 
+echo "Uploading file $1 parts to Elastic Search.."
+for i in $1-part-*; 
+do 
+   echo "Uploading file $i"
+   # "head -c100" ensures we only see first 100 characters of result, making it easier to see what happens
+   curl -r 0-199- -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/_bulk --data-binary "@$i" | head -c100
+done
 
 echo "Upload ended."
