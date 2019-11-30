787b454b49a1d33d7135ee26458b8c6c51396064
Julius vanaf Macbook Air <juliuszelf@gmail.com>
2019-11-30 10:04:49 +0100

    flask templates, bootstrap

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,24 +1,15 @@
 # flask-app/app.py
 
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
 es = Elasticsearch('http://es01:9200')
 
-
-search_page = """
-    <form>
-      <label for="site-search">Global charity search:</label>
-      <input type="search" id="site-search" name="search" aria-label="Search charities">
-      <button>Search</button>
-    </form><br>
-    <hr><br>
-    """  
-
 @app.route("/", methods=["GET", "POST"])
 def home():
+    title = "Global charity search engine"
     try:
         search_value = request.args.to_dict()['search']
         res=es.search(index='chars', body={'query':{ 'match': {'Name': search_value}}})
@@ -37,14 +28,15 @@ def home():
 
             results_text += result_text + "<hr><br>"
 
-        return search_page \
-                + "searched for: " + search_value + "<br><br>" \
-                + "nr results: " + str(nr_results_shown) + "<br><br>" \
-                + "<br><hr><br>" + str(results_text)
+        content = "Searched for: " + search_value + "<br>" \
+                + "Results (" + str(nr_results_shown) + ")<br>" \
+                + "<hr>" + str(results_text)
+        return render_template("main.html", title=title, content=content)
     except KeyError:
         print("no search")
 
-    return search_page
+    return render_template("main.html", title=title)
+    # return search_page
 
 
 if __name__ == "__main__":
