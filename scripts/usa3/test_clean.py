96c0d5c71f0d3557b1589f0b5d15ec367786cacd
Julius Huijnk <julius@immer.im>
2019-12-18 23:19:34 +0100

    test_clean same for all countries

---
+++ scripts/usa3/test_clean.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+import unittest
+import os
+# noinspection PyUnresolvedReferences
+from cleanCSV_parser import parse
+
+
+class CleanTestCase(unittest.TestCase):
+
+    def setUp(self):
+        print("setup")
+        pass
+
+    def tearDown(self):
+        print("teardown")
+        pass
+
+    def test_head(self):
+        source = 'testdata/raw.head.test.csv'
+        output = 'testoutput/clean.head.csv'
+        expected = 'testdata/clean.head.expected.csv'
+
+        parse(source, output)
+
+        # Now we should have created a file, validate it is equal to expected result.
+        with open(output) as f:
+            content_output = f.read()
+
+        with open(expected) as f:
+            expected_output = f.read()
+
+        self.assertEqual(content_output, expected_output)
+
+        # cleanup
+        os.remove(output)
